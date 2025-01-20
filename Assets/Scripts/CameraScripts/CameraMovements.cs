39c22cc61809646c058bcbe8fe3386f0a77d7a19
Tahmid Nasif Tamal <tahmidtamal.200026.nasif@gmail.com>
2025-01-20 10:49:26 +0600

    Issues Fixed

--- Assets/Scripts/CameraScripts/CameraMovements.cs
+++ Assets/Scripts/CameraScripts/CameraMovements.cs
@@ -1,17 +1,25 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 namespace CameraBehaves
 {
     public class CameraMovement : MonoBehaviour
     {
-        public Transform target;
+        private Transform target; // Private field
+
+        // Public property for encapsulation
+        public Transform Target
+        {
+            get => target;
+            set => target = value;
+        }
+
         void Start()
         {
             // This method is intentionally left empty.
             // It serves as a placeholder for future initialization logic, if required.
         }
+
         void Update()
         {
             if (target != null)
